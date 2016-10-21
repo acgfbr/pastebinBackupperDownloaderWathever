06a02442913622615c7ee24d0490ab3de837478e
Tonepanetone <dh.18@msn.com>
2016-10-21 09:21:16 -0200

    Revert "another little fix"
    This reverts commit 1dac72bfdb3e026fe32e5cb700644dd70306cb64.

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ class main:
 
         nome_do_arquivo = str(split[1])
 
-        
+        f = open('Downloads/' + str(split[1]) + '.txt', 'w')
+
         r = requests.get(link)  # faz um novo get no link recebido
 
         soup = BeautifulSoup(r.content, 'lxml')
@@ -43,10 +44,9 @@ class main:
 
         for i in range(18,list_len) :
             if(len(tbl[i].contents) > 0) :
-                 if(len(tbl[i].contents[0]) > 0) :
-                    f = open('Downloads/' + str(split[1]) + '.txt', 'w')
-                    f.write(''.join((tbl[i].contents[0].contents[0]).encode('utf-8').strip()))
-                    f.write('\n')
+             if(len(tbl[i].contents[0]) > 0) :
+                f.write(''.join((tbl[i].contents[0].contents[0]).encode('utf-8').strip()))
+                f.write('\n')
             
         f.close()
         
